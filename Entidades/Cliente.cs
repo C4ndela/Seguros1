80f8fce7f9f5c1e56f9ee17ba217f09f742e91b0
candela contreras <candecon08@hotmail.com>
2022-11-10 15:42:40 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Cliente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+     public class Cliente
+     {
+        #region atributos
+        private int DNI;
+        private string nombre;
+        private string apellido;
+        private string localidad;
+        private string domicilio;
+        private long nroTelefonico;
+        #endregion
+
+        #region constructor
+        public Cliente()
+        { }
+        public Cliente (int dni, string nom, string apelli, string local, string domi, long nroTel)
+        {
+            DNI = dni;
+            nombre = nom;
+            apellido = apelli; 
+            localidad = local; 
+            domicilio = domi;   
+            nroTelefonico = nroTel;
+        }
+        #endregion
+
+
+
+     }
+}
---
+++ Entidades/Vehiculo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Vehiculo
+    {
+        #region atributos
+        private string Patente;
+        private string Modelo;
+        private int año;
+        private int sumaAsegurada;
+        private bool asegurado;
+        #endregion 
+
+        #region constructor
+        public Vehiculo()
+        { }
+        public Vehiculo(string patente, string modelo, int anio, int sumAsegurada)
+        {
+            Patente = patente;
+            Modelo = modelo;
+            año = anio;
+            sumaAsegurada = sumAsegurada;
+            asegurado = false;
+        }
+        #endregion
+
+
+    }
+}
